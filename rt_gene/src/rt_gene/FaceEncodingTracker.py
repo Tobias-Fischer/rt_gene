b8684d7b7c549221d6938b8cfd3183584c76da26
Ahmed Al-Hindawi <a.al-hindawi@imperial.ac.uk>
2019-06-03 09:30:39 +0100

    added authorship

--- rt_gene/src/rt_gene/FaceEncodingTracker.py
+++ rt_gene/src/rt_gene/FaceEncodingTracker.py
@@ -1,5 +1,6 @@
 """
 @Kevin Cortacero <cortacero.k31130@gmail.com>
+@Ahmed Al-Hindawi <a.al-hindawi@imperial.ac.uk>
 Licensed under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode)
 """
 from __future__ import print_function
