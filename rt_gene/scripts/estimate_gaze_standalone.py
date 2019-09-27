763faa861ef063f1432888b6194822e92bf7d7f6
Tobias-Fischer <t.fischer@imperial.ac.uk>
2019-09-27 16:53:51 +0100

    Simplify estimate_gaze_standalone to use restructured base classes

--- rt_gene/scripts/estimate_gaze_standalone.py
+++ rt_gene/scripts/estimate_gaze_standalone.py
@@ -7,17 +7,11 @@ import argparse
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-import tensorflow
-from keras.backend.tensorflow_backend import set_session
-from keras.models import load_model
+from tqdm import tqdm
 
-from face_alignment.detection.sfd import FaceDetector
-
-from rt_gene.gaze_tools import crop_face_from_image, accuracy_angle, angle_loss, get_phi_theta_from_euler
-from rt_gene.ThreeDDFA.inference import parse_roi_box_from_bbox, parse_roi_box_from_landmark
-from rt_gene.extract_landmarks_method import LandmarkMethod
-from rt_gene.tracker_generic import TrackedSubject
-from estimate_gaze import GazeEstimator
+from rt_gene.gaze_tools import get_phi_theta_from_euler
+from rt_gene.extract_landmarks_method_base import LandmarkMethodBase
+from rt_gene.estimate_gaze_base import GazeEstimatorBase
 
 
 def load_camera_calibration(calibration_file):
@@ -31,75 +25,61 @@ def load_camera_calibration(calibration_file):
     return dist_coefficients, camera_matrix
 
 
-def estimate_gaze(base_name, color_img, dist_coefficients, camera_matrix):
-    print('Extracting face bounding boxes')
-    faceboxes = LandmarkMethod.get_face_bb(face_net, color_img)
-    if len(faceboxes) == 0:
-        print('Could not find faces in the image')
-        return
-    face_images = [crop_face_from_image(color_img, b) for b in faceboxes]
+def rotation_vector_to_rpy(rotation_vector):
+    roll_pitch_yaw = np.array([-rotation_vector[2], rotation_vector[1], rotation_vector[0]])
+    roll_pitch_yaw[0] += np.pi
+    if roll_pitch_yaw[0] > np.pi:
+        roll_pitch_yaw[0] -= 2 * np.pi
+    return roll_pitch_yaw
 
-    print('Extracting landmark positions')
-    subjects = []
-    for facebox, face_image in zip(faceboxes, face_images):
-        roi_box = parse_roi_box_from_bbox(facebox)
-        initial_pts68 = LandmarkMethod.ddfa_forward_pass(facial_landmark_nn, color_img, roi_box)
-        roi_box_refined = parse_roi_box_from_landmark(initial_pts68)
-        pts68 = LandmarkMethod.ddfa_forward_pass(facial_landmark_nn, color_img, roi_box_refined)
 
-        np_landmarks = np.array((pts68[0], pts68[1])).T
-        transformed_landmarks = LandmarkMethod.transform_landmarks(np_landmarks, facebox)
-        subjects.append(TrackedSubject(np.array(facebox), face_image, transformed_landmarks, np_landmarks))
-    print('Extracted landmarks for {} subject(s)'.format(len(subjects)))
-
-    print('Extracting eye image patches')
-    eye_image_size = (60, 36)
-
+def extract_eye_image_patches(subjects):
     for subject in subjects:
-        le_c, re_c, le_bb, re_bb = subject.get_eye_image_from_landmarks(subject.transformed_landmarks, subject.face_color, eye_image_size)
+        le_c, re_c, le_bb, re_bb = subject.get_eye_image_from_landmarks(subject.transformed_landmarks, subject.face_color, landmark_estimator.eye_image_size)
         subject.left_eye_color = le_c
         subject.right_eye_color = re_c
         subject.left_eye_bb = le_bb
         subject.right_eye_bb = re_bb
 
-    print('Getting head pose')
-    rvec_init = np.array([[0.01891013], [0.08560084], [-3.14392813]])
-    tvec_init = np.array([[-14.97821226], [-10.62040383], [-2053.03596872]])
 
-    model_size_rescale = 30.0
-    interpupillary_distance = 0.058
-    model_points = LandmarkMethod.get_full_model_points(interpupillary_distance, model_size_rescale)
+def estimate_gaze(base_name, color_img, dist_coefficients, camera_matrix):
+    faceboxes = landmark_estimator.get_face_bb(color_img)
+    if len(faceboxes) == 0:
+        tqdm.write('Could not find faces in the image')
+        return
+
+    subjects = landmark_estimator.get_subjects_from_faceboxes(color_img, faceboxes)
+    extract_eye_image_patches(subjects)
 
     for idx, subject in enumerate(subjects):
-        (success, rotation_vector, translation_vector) = cv2.solvePnP(model_points, subject.marks, cameraMatrix=camera_matrix,
+        (success, rotation_vector, translation_vector) = cv2.solvePnP(landmark_estimator.model_points, subject.marks, cameraMatrix=camera_matrix,
                                                                       distCoeffs=dist_coefficients, flags=cv2.SOLVEPNP_ITERATIVE,
-                                                                      useExtrinsicGuess=True, rvec=rvec_init, tvec=tvec_init)
-        roll_pitch_yaw = np.array([-rotation_vector[2], rotation_vector[1], rotation_vector[0]])
-        roll_pitch_yaw[0] += np.pi
-        if roll_pitch_yaw[0] > np.pi:
-            roll_pitch_yaw[0] -= 2 * np.pi
+                                                                      useExtrinsicGuess=True,
+                                                                      rvec=landmark_estimator.rvec_init.copy(),
+                                                                      tvec=landmark_estimator.tvec_init.copy())
+        roll_pitch_yaw = rotation_vector_to_rpy(rotation_vector)
 
         if roll_pitch_yaw is not None:
             phi_head, theta_head = get_phi_theta_from_euler(roll_pitch_yaw)
 
             face_image_resized = cv2.resize(subject.face_color, dsize=(224, 224), interpolation=cv2.INTER_CUBIC)
-            head_pose_image = LandmarkMethod.visualize_headpose_result(face_image_resized, (phi_head, theta_head))
+            head_pose_image = landmark_estimator.visualize_headpose_result(face_image_resized, (phi_head, theta_head))
 
             if args.vis_headpose:
                 plt.axis("off")
                 plt.imshow(cv2.cvtColor(head_pose_image, cv2.COLOR_BGR2RGB))
                 plt.show()
         else:
-            print('Not able to extract head pose for subject {}'.format(idx))
+            tqdm.write('Not able to extract head pose for subject {}'.format(idx))
             continue
 
-        print('Estimating gaze')
-        input_r = GazeEstimator.input_from_image(subject.right_eye_color)
-        input_l = GazeEstimator.input_from_image(subject.left_eye_color)
-        gaze_est = GazeEstimator.estimate_gaze_twoeyes_standalone(input_l, input_r,
-                                                                  np.array([theta_head, phi_head]), graph, models)
-        r_gaze_img = GazeEstimator.visualize_eye_result(subject.right_eye_color, gaze_est)
-        l_gaze_img = GazeEstimator.visualize_eye_result(subject.left_eye_color, gaze_est)
+        input_r = gaze_estimator.input_from_image(subject.right_eye_color)
+        input_l = gaze_estimator.input_from_image(subject.left_eye_color)
+        gaze_est = gaze_estimator.estimate_gaze_twoeyes(input_l, input_r, np.array([theta_head, phi_head]))
+
+        # Build visualizations
+        r_gaze_img = gaze_estimator.visualize_eye_result(subject.right_eye_color, gaze_est)
+        l_gaze_img = gaze_estimator.visualize_eye_result(subject.left_eye_color, gaze_est)
         s_gaze_img = np.concatenate((r_gaze_img, l_gaze_img), axis=1)
 
         if args.vis_gaze:
@@ -138,27 +118,11 @@ if __name__ == '__main__':
         print('Provide either a path to an image or a path to a directory containing images')
         sys.exit(1)
 
-    print('Loading networks')
-    face_net = FaceDetector(device='cuda:0')
-    facial_landmark_nn = LandmarkMethod.load_face_landmark_model()
-
-    with tensorflow.device("/gpu:0"):
-        config = tensorflow.ConfigProto(inter_op_parallelism_threads=1, intra_op_parallelism_threads=1)
-        config.gpu_options.allow_growth = True
-        config.gpu_options.per_process_gpu_memory_fraction = 0.3
-        config.log_device_placement = False
-        sess = tensorflow.Session(config=config)
-        set_session(sess)
-
-    models = []
-    for model_file in args.models:
-        print('Load model ' + model_file)
-        model = load_model(model_file, custom_objects={'accuracy_angle': accuracy_angle, 'angle_loss': angle_loss})
-        # noinspection PyProtectedMember
-        model._make_predict_function()  # have to initialize before threading
-        models.append(model)
-
-    graph = tensorflow.get_default_graph()
+    tqdm.write('Loading networks')
+    landmark_estimator = LandmarkMethodBase(device_id_facedetection="cuda:0",
+                                            checkpoint_path_landmark="../model_nets/phase1_wpdc_vdc.pth.tar",
+                                            model_points_file="../model_nets/face_model_68.txt")
+    gaze_estimator = GazeEstimatorBase(device_id_gaze="/gpu:0", model_files=args.models)
 
     if not os.path.isdir(args.output_path):
         os.makedirs(args.output_path)
@@ -166,13 +130,13 @@ if __name__ == '__main__':
     for image_filename in image_path_list:
         image = cv2.imread(os.path.join(args.im_path, image_filename))
         if image is None:
-            print('Could not load ' + image_filename + ', skipping this image.')
+            tqdm.write('Could not load ' + image_filename + ', skipping this image.')
 
         if args.calib_file is not None:
             _dist_coefficients, _camera_matrix = load_camera_calibration(args.calib_file)
         else:
             im_width, im_height = image.shape[1], image.shape[0]
-            print('WARNING!!! You should provide the camera calibration file, otherwise you might get bad results. Using a crude approximation!')
+            tqdm.write('WARNING!!! You should provide the camera calibration file, otherwise you might get bad results. Using a crude approximation!')
             _dist_coefficients, _camera_matrix = np.zeros((1, 5)), np.array([[im_height / 1.05, 0.0, im_width / 2.0], [0.0, im_height / 1.05, im_height / 2.0], [0.0, 0.0, 1.0]])
 
         estimate_gaze(image_filename, image, _dist_coefficients, _camera_matrix)
