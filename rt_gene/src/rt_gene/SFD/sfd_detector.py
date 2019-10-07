5f1af23df7d5e0710e935d0e620e4e4236519dfa
Ahmed Al-Hindawi <a.al-hindawi@imperial.ac.uk>
2019-10-07 14:30:15 +0100

    Removed face_alignment as a dependency and imported S3FD as a facedetector within RT-GENE Made the face-encoding face tracker the default if no "use_face_encoding_tracker" variable is declared

---
+++ rt_gene/src/rt_gene/SFD/sfd_detector.py
@@ -0,0 +1,129 @@
+import torch
+import torch.nn.functional as F
+import cv2
+import numpy as np
+from .net_s3fd import s3fd
+
+
+class SFDDetector(object):
+
+    __WHITENING = np.array([104, 117, 123])
+
+    def __init__(self, device, path_to_detector=None):
+        self.device = device
+
+        if path_to_detector is None:
+            import rospkg
+            path_to_detector = rospkg.RosPack().get_path('rt_gene') + '/model_nets/SFD/s3fd_facedetector.pth'
+
+        if 'cuda' in device:
+            torch.backends.cudnn.benchmark = True
+
+        self.face_detector = s3fd()
+        self.face_detector.load_state_dict(torch.load(path_to_detector))
+        self.face_detector.to(device)
+        self.face_detector.eval()
+
+    def detect_from_image(self, tensor_or_path):
+        image = self.tensor_or_path_to_ndarray(tensor_or_path)
+
+        bboxlist = self.detect(self.face_detector, image, device=self.device)
+        keep = self.nms(bboxlist, 0.3)
+        bboxlist = bboxlist[keep, :]
+        bboxlist = [x for x in bboxlist if x[-1] > 0.5]
+
+        return bboxlist
+
+    @staticmethod
+    def tensor_or_path_to_ndarray(tensor_or_path, rgb=True):
+        """Convert path (represented as a string) or torch.tensor to a numpy.ndarray
+
+        Arguments:
+            tensor_or_path {numpy.ndarray, torch.tensor or string} -- path to the image, or the image itself
+        """
+        if isinstance(tensor_or_path, str):
+            return cv2.imread(tensor_or_path) if not rgb else io.imread(tensor_or_path)
+        elif torch.is_tensor(tensor_or_path):
+            # Call cpu in case its coming from cuda
+            return tensor_or_path.cpu().numpy()[..., ::-1].copy() if not rgb else tensor_or_path.cpu().numpy()
+        elif isinstance(tensor_or_path, np.ndarray):
+            return tensor_or_path[..., ::-1].copy() if not rgb else tensor_or_path
+        else:
+            raise TypeError
+
+    @staticmethod
+    def nms(dets, thresh):
+        if 0 == len(dets):
+            return []
+        x1, y1, x2, y2, scores = dets[:, 0], dets[:, 1], dets[:, 2], dets[:, 3], dets[:, 4]
+        areas = (x2 - x1 + 1) * (y2 - y1 + 1)
+        order = scores.argsort()[::-1]
+
+        keep = []
+        while order.size > 0:
+            i = order[0]
+            keep.append(i)
+            xx1, yy1 = np.maximum(x1[i], x1[order[1:]]), np.maximum(y1[i], y1[order[1:]])
+            xx2, yy2 = np.minimum(x2[i], x2[order[1:]]), np.minimum(y2[i], y2[order[1:]])
+
+            w, h = np.maximum(0.0, xx2 - xx1 + 1), np.maximum(0.0, yy2 - yy1 + 1)
+            ovr = w * h / (areas[i] + areas[order[1:]] - w * h)
+
+            inds = np.where(ovr <= thresh)[0]
+            order = order[inds + 1]
+
+        return keep
+
+    @staticmethod
+    def decode(loc, priors, variances):
+        """Decode locations from predictions using priors to undo
+        the encoding we did for offset regression at train time.
+        Args:
+            loc (tensor): location predictions for loc layers,
+                Shape: [num_priors,4]
+            priors (tensor): Prior boxes in center-offset form.
+                Shape: [num_priors,4].
+            variances: (list[float]) Variances of priorboxes
+        Return:
+            decoded bounding box predictions
+        """
+
+        boxes = torch.cat((
+            priors[:, :2] + loc[:, :2] * variances[0] * priors[:, 2:],
+            priors[:, 2:] * torch.exp(loc[:, 2:] * variances[1])), 1)
+        boxes[:, :2] -= boxes[:, 2:] / 2
+        boxes[:, 2:] += boxes[:, :2]
+        return boxes
+
+    def detect(self, net, img, device):
+        img = img - self.__WHITENING
+        img = img.transpose(2, 0, 1)
+        img = img.reshape((1,) + img.shape)
+
+        img = torch.from_numpy(img).float().to(device)
+        with torch.no_grad():
+            olist = net(img)
+
+        bboxlist = []
+        for i in range(len(olist) // 2):
+            olist[i * 2] = F.softmax(olist[i * 2], dim=1)
+        olist = [oelem.data.cpu() for oelem in olist]
+        for i in range(len(olist) // 2):
+            ocls, oreg = olist[i * 2], olist[i * 2 + 1]
+            stride = 2 ** (i + 2)  # 4,8,16,32,64,128
+
+            poss = zip(*np.where(ocls[:, 1, :, :] > 0.05))
+            for Iindex, hindex, windex in poss:
+                axc, ayc = stride / 2 + windex * stride, stride / 2 + hindex * stride
+                score = ocls[0, 1, hindex, windex]
+                loc = oreg[0, :, hindex, windex].contiguous().view(1, 4)
+                priors = torch.Tensor([[axc / 1.0, ayc / 1.0, stride * 4 / 1.0, stride * 4 / 1.0]])
+                variances = [0.1, 0.2]
+                box = self.decode(loc, priors, variances)
+                x1, y1, x2, y2 = box[0] * 1.0
+                bboxlist.append([x1, y1, x2, y2, score])
+        bboxlist = np.array(bboxlist)
+        if 0 == len(bboxlist):
+            bboxlist = np.zeros((1, 5))
+
+        return bboxlist
