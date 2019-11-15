fixed visualisation of gaze due to publishing in camera space rather than ros space implemented tf2 rather than tf averaging of latency for human readability

rt_gene/scripts/estimate_gaze.py
@@ -13,13 +13,15 @@ from __future__ import print_function, division, absolute_import
 import os
 import numpy as np
 from tqdm import tqdm
+import collections
 
 import rospkg
 import rospy
+from geometry_msgs.msg import TransformStamped
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
-from tf import TransformBroadcaster, TransformListener
-import tf.transformations
+import tf2_ros
+from tf import transformations
 
 import rt_gene.gaze_tools as gaze_tools
 from rt_gene.subject_ros_bridge import SubjectListBridge
@@ -33,12 +35,13 @@ class GazeEstimatorROS(GazeEstimatorBase):
         self.bridge = CvBridge()
         self.subjects_bridge = SubjectListBridge()
 
-        self.tf_broadcaster = TransformBroadcaster()
-        self.tf_listener = TransformListener()
+        self.tf2_broadcaster = tf2_ros.TransformBroadcaster()
+        self.tf2_buffer = tf2_ros.Buffer()
+        self.tf2_listener = tf2_ros.TransformListener(self.tf2_buffer)
 
         self.tf_prefix = rospy.get_param("~tf_prefix", "gaze")
         self.headpose_frame = self.tf_prefix + "/head_pose_estimated"
-        self.ros_tf_frame = rospy.get_param("~ros_tf_frame", "/kinect2_ros_frame")
+        self.ros_tf_frame = rospy.get_param("~ros_tf_frame", "kinect2_ros_frame")
 
         self.image_subscriber = rospy.Subscriber("/subjects/images", MSG_SubjectImagesList, self.image_callback, queue_size=3, buff_size=2 ** 24)
         self.subjects_gaze_img = rospy.Publisher("/subjects/gazeimages", Image, queue_size=3)
@@ -46,6 +49,8 @@ class GazeEstimatorROS(GazeEstimatorBase):
         self.visualise_eyepose = rospy.get_param("~visualise_eyepose", default=True)
 
         self._last_time = rospy.Time().now()
+        self._freq_deque = collections.deque(maxlen=30)  # average frequency statistic over roughly one second
+        self._latency_deque = collections.deque(maxlen=30)
 
     def publish_image(self, image, image_publisher, timestamp):
         """This image publishes the `image` to the `image_publisher` with the given `timestamp`."""
@@ -68,8 +73,9 @@ class GazeEstimatorROS(GazeEstimatorBase):
         valid_subject_list = []
         for subject_id, s in subjects_dict.items():
             try:
-                (trans_head, rot_head) = self.tf_listener.lookupTransform(self.ros_tf_frame, self.headpose_frame + str(subject_id), timestamp)
-                euler_angles_head = list(tf.transformations.euler_from_quaternion(rot_head))
+                transform_msg = self.tf2_buffer.lookup_transform(self.ros_tf_frame, self.headpose_frame + str(subject_id), timestamp)
+                rot_head = transform_msg.transform.rotation
+                euler_angles_head = list(transformations.euler_from_quaternion([rot_head.x, rot_head.y, rot_head.z, rot_head.w]))
                 euler_angles_head = gaze_tools.limit_yaw(euler_angles_head)
 
                 phi_head, theta_head = gaze_tools.get_phi_theta_from_euler(euler_angles_head)
@@ -77,7 +83,7 @@ class GazeEstimatorROS(GazeEstimatorBase):
                 input_r_list.append(self.input_from_image(s.right))
                 input_l_list.append(self.input_from_image(s.left))
                 valid_subject_list.append(subject_id)
-            except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException, tf.Exception):
+            except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException, tf2_ros.TransformException):
                 pass
 
         if len(valid_subject_list) == 0:
@@ -105,21 +111,39 @@ class GazeEstimatorROS(GazeEstimatorBase):
 
         _now = rospy.Time().now()
         _freq = 1.0 / (_now - self._last_time).to_sec()
+        self._freq_deque.append(_freq)
+        self._latency_deque.append(_now.to_sec() - timestamp.to_sec())
         self._last_time = _now
         tqdm.write(
-            '\033[2K\033[1;32mTime now: {:.2f} message color: {:.2f} diff: {:.2f}s for {} subjects {:.0f}Hz\033[0m'.format((_now.to_sec()), timestamp.to_sec(),
-                                                                                                                          _now.to_sec() - timestamp.to_sec(),
-                                                                                                                          len(valid_subject_list),
-                                                                                                                          _freq), end="\r")
+            '\033[2K\033[1;32mTime now: {:.2f} message color: {:.2f} latency: {:.2f}s for {} subject(s) {:.0f}Hz\033[0m'.format(
+                (_now.to_sec()), timestamp.to_sec(), np.mean(self._latency_deque), len(valid_subject_list), np.mean(self._freq_deque)), end="\r")
 
     def publish_gaze(self, est_gaze, msg_stamp, subject_id):
         """Publish the gaze vector as a PointStamped."""
         theta_gaze = est_gaze[0]
         phi_gaze = est_gaze[1]
         euler_angle_gaze = gaze_tools.get_euler_from_phi_theta(phi_gaze, theta_gaze)
-        quaternion_gaze = tf.transformations.quaternion_from_euler(*euler_angle_gaze)
-        self.tf_broadcaster.sendTransform((0, 0, 0.05),  # publish it 5cm above the head pose's origin (nose tip)
-                                          quaternion_gaze, msg_stamp, self.tf_prefix + "/world_gaze" + str(subject_id), self.headpose_frame + str(subject_id))
+        quaternion_gaze = transformations.quaternion_from_euler(*euler_angle_gaze)
+
+        t = TransformStamped()
+        t.header.stamp = msg_stamp
+        t.header.frame_id = self.headpose_frame + str(subject_id)
+        t.child_frame_id = self.tf_prefix + "/world_gaze" + str(subject_id)
+        t.transform.translation.x = 0
+        t.transform.translation.y = 0
+        t.transform.translation.z = 0.05  # publish it 5cm above the head pose's origin (nose tip)
+        t.transform.rotation.x = quaternion_gaze[0]
+        t.transform.rotation.y = quaternion_gaze[1]
+        t.transform.rotation.z = quaternion_gaze[2]
+        t.transform.rotation.w = quaternion_gaze[3]
+
+        try:
+            self.tf2_broadcaster.sendTransform([t])
+        except rospy.ROSException as exc:
+            if str(exc) == "publish() to a closed topic":
+                pass
+            else:
+                raise exc
 
 
 if __name__ == "__main__":