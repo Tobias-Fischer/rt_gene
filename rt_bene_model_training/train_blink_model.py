34cb827a1cbea4eb5d82cadb55596015b9d19c38
Tobias Fischer <info@tobiasfischer.info>
2020-02-13 15:47:56 +1000

    General cleanup train_blink_model

--- rt_bene_model_training/train_blink_model.py
+++ rt_bene_model_training/train_blink_model.py
@@ -1,34 +1,25 @@
 #!/usr/bin/env python
 
-# from https://hackernoon.com/tf-serving-keras-mobilenetv2-632b8d92983c
-
-from dataset_manager import RTBeneDataset
-
-from blink_model_factory import create_model
-from tensorflow.keras.callbacks import ModelCheckpoint
-
-from tqdm import tqdm
-
 import gc
-
 import argparse
 
-import numpy as np
-
-import time
-import json
-
 import tensorflow as tf
+from tensorflow.keras.callbacks import ModelCheckpoint
 
-config = tf.ConfigProto()
+from dataset_manager import RTBeneDataset
+from blink_model_factory import create_model
+
+tf.compat.v1.disable_eager_execution()
+
+config = tf.compat.v1.ConfigProto()
 config.gpu_options.allow_growth = True
-tf.keras.backend.set_session(tf.Session(config=config))
+tf.compat.v1.keras.backend.set_session(tf.compat.v1.Session(config=config))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("model_base", choices=['densenet121', 'resnet50', 'mobilenetv2'])
     parser.add_argument("model_path", help="target folder to save the models (auto-saved)")
-    parser.add_argument("csv_subjects", help="path to the dataset csv file")
+    parser.add_argument("csv_subject_list", help="path to the dataset csv file")
     parser.add_argument("--use_weight_balancing", help="whether to use weights")
     parser.add_argument("--batch_size", type=int, default=64)
     parser.add_argument("--epochs", type=int, default=8)
@@ -41,23 +32,27 @@ if __name__ == "__main__":
     input_size = args.input_size
     use_weight_balancing = args.use_weight_balancing
 
-    dataset = RTBeneDataset(args.csv_subjects, input_size)
-    dataset.load()
-    fold_infos = [([0, 1], 'fold1'), ([0, 2], 'fold2'), ([1, 2], 'fold3')]
+    dataset = RTBeneDataset(args.csv_subject_list, input_size)
+    fold_infos = [
+        ([0, 1], 'fold1'),
+        ([0, 2], 'fold2'),
+        ([1, 2], 'fold3')
+    ]
 
-    validation_set = dataset.get_validation_set()
+    validation_set = dataset.get_validation_data()
 
     # 3 folds training
     for subjects_train, fold_name in fold_infos:
-        training_fold = dataset.get_folds(subjects_train)
+        training_fold = dataset.get_training_data(subjects_train)
         positive = training_fold['positive']
         negative = training_fold['negative']
 
-        print('Number of positive samples in training data: {} ({:.2f}% of total)'.format(positive, 100 * float(positive) / len(training_fold['y'])))
+        print('Number of positive samples in training data: {} ({:.2f}% of total)'.
+              format(positive, 100 * float(positive) / len(training_fold['y'])))
 
         metrics = [tf.keras.metrics.BinaryAccuracy(), tf.keras.metrics.Recall(), tf.keras.metrics.Precision()]
-        model, name = create_model(args.model_base, [input_size[0], input_size[1], 3], 1e-4, metrics)
-        name = 'rt-bene_' + name + '_' + fold_name
+        model = create_model(args.model_base, [input_size[0], input_size[1], 3], 1e-4, metrics)
+        name = 'rt-bene_' + args.model_base + '_' + fold_name
 
         if use_weight_balancing:
             weight_for_0 = 1. / negative * (negative + positive)
