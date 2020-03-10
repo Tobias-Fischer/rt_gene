cc2f9251b27ceb786575547accb1ce2fee915d3c
Ahmed Al-Hindawi <a.al-hindawi@imperial.ac.uk>
2020-03-10 14:42:36 +0000

    added more models

--- rt_gene/src/rt_gene/gaze_estimation_models_pytorch.py
+++ rt_gene/src/rt_gene/gaze_estimation_models_pytorch.py
@@ -35,6 +35,90 @@ class GazeEstimationAbstractModel(nn.Module):
                 nn.init.zeros_(m.bias)
 
 
+class GazeEstimationModelShufflenet(GazeEstimationAbstractModel):
+
+    def __init__(self, num_out=2):
+        super(GazeEstimationModelShufflenet, self).__init__()
+        _left_model = models.shufflenet_v2_x1_0(pretrained=True)
+        _right_model = models.shufflenet_v2_x1_0(pretrained=True)
+
+        self.left_features = nn.Sequential(
+            _left_model.conv1,
+            _left_model.maxpool,
+            _left_model.stage2,
+            _left_model.stage3,
+            _left_model.stage4,
+            _left_model.conv5
+        )
+
+        self.right_features = nn.Sequential(
+            _right_model.conv1,
+            _right_model.maxpool,
+            _right_model.stage2,
+            _right_model.stage3,
+            _right_model.stage4,
+            _right_model.conv5
+        )
+
+        for param in self.left_features.parameters():
+            param.requires_grad = True
+        for param in self.right_features.parameters():
+            param.requires_grad = True
+
+        _num_ftrs = _left_model.fc.in_features + _right_model.fc.in_features + 2  # left, right and head_pose
+        self.classifier = GazeEstimationAbstractModel._create_fc_layers(num_out=num_out, num_features=_num_ftrs)
+        GazeEstimationAbstractModel._init_weights(self.modules())
+
+    def forward(self, left_eye, right_eye, headpose):
+        left_x = self.left_features(left_eye)
+        left_x = left_x.mean([2, 3])
+
+        right_x = self.right_features(right_eye)
+        right_x = right_x.mean([2, 3])
+
+        concat = torch.cat((left_x, right_x, headpose), dim=1)
+
+        fc_output = self.classifier(concat)
+
+        return fc_output
+
+
+class GazeEstimationModelMnas(GazeEstimationAbstractModel):
+
+    def __init__(self, num_out=2):
+        super(GazeEstimationModelMnas, self).__init__()
+        _left_model = models.mnasnet1_0(pretrained=True)
+        _right_model = models.mnasnet1_0(pretrained=True)
+
+        _left_features = [module for module in _left_model.layers.children()]
+        self.left_features = nn.Sequential(*_left_features)
+
+        _right_features = [module for module in _right_model.layers.children()]
+        self.right_features = nn.Sequential(*_right_features)
+
+        for param in self.left_features.parameters():
+            param.requires_grad = True
+        for param in self.right_features.parameters():
+            param.requires_grad = True
+
+        _num_ftrs = 1280 + 1280 + 2  # left, right and head_pose
+        self.classifier = GazeEstimationAbstractModel._create_fc_layers(num_out=num_out, num_features=_num_ftrs)
+        GazeEstimationAbstractModel._init_weights(self.modules())
+
+    def forward(self, left_eye, right_eye, headpose):
+        left_x = self.left_features(left_eye)
+        left_x = left_x.mean([2, 3])
+
+        right_x = self.right_features(right_eye)
+        right_x = right_x.mean([2, 3])
+
+        concat = torch.cat((left_x, right_x, headpose), dim=1)
+
+        fc_output = self.classifier(concat)
+
+        return fc_output
+
+
 class GazeEstimationModelResnet50(GazeEstimationAbstractModel):
 
     def __init__(self, num_out=2):  # phi, theta
@@ -44,27 +128,27 @@ class GazeEstimationModelResnet50(GazeEstimationAbstractModel):
 
         # remove the last ConvBRelu layer
         self.left_features = nn.Sequential(
-            *list([_left_model.conv1,
-                   _left_model.bn1,
-                   _left_model.relu,
-                   _left_model.maxpool,
-                   _left_model.layer1,
-                   _left_model.layer2,
-                   _left_model.layer3,
-                   _left_model.layer4,
-                   _left_model.avgpool])
+            _left_model.conv1,
+            _left_model.bn1,
+            _left_model.relu,
+            _left_model.maxpool,
+            _left_model.layer1,
+            _left_model.layer2,
+            _left_model.layer3,
+            _left_model.layer4,
+            _left_model.avgpool
         )
 
         self.right_features = nn.Sequential(
-            *list([_right_model.conv1,
-                   _right_model.bn1,
-                   _right_model.relu,
-                   _right_model.maxpool,
-                   _right_model.layer1,
-                   _right_model.layer2,
-                   _right_model.layer3,
-                   _right_model.layer4,
-                   _right_model.avgpool])
+            _right_model.conv1,
+            _right_model.bn1,
+            _right_model.relu,
+            _right_model.maxpool,
+            _right_model.layer1,
+            _right_model.layer2,
+            _right_model.layer3,
+            _right_model.layer4,
+            _right_model.avgpool
         )
 
         for param in self.left_features.parameters():
@@ -92,34 +176,89 @@ class GazeEstimationModelResnet50(GazeEstimationAbstractModel):
 
 class GazeEstimationmodelResnet18(GazeEstimationAbstractModel):
 
-    def __init__(self, num_out=3):
+    def __init__(self, num_out=2):
         super(GazeEstimationmodelResnet18, self).__init__()
         _left_model = models.resnet18(pretrained=True)
         _right_model = models.resnet18(pretrained=True)
 
         # remove the last ConvBRelu layer
         self.left_features = nn.Sequential(
-            *list([_left_model.conv1,
-                   _left_model.bn1,
-                   _left_model.relu,
-                   _left_model.maxpool,
-                   _left_model.layer1,
-                   _left_model.layer2,
-                   _left_model.layer3,
-                   _left_model.layer4,
-                   _left_model.avgpool])
+            _left_model.conv1,
+            _left_model.bn1,
+            _left_model.relu,
+            _left_model.maxpool,
+            _left_model.layer1,
+            _left_model.layer2,
+            _left_model.layer3,
+            _left_model.layer4,
+            _left_model.avgpool
         )
 
         self.right_features = nn.Sequential(
-            *list([_right_model.conv1,
-                   _right_model.bn1,
-                   _right_model.relu,
-                   _right_model.maxpool,
-                   _right_model.layer1,
-                   _right_model.layer2,
-                   _right_model.layer3,
-                   _right_model.layer4,
-                   _right_model.avgpool])
+            _right_model.conv1,
+            _right_model.bn1,
+            _right_model.relu,
+            _right_model.maxpool,
+            _right_model.layer1,
+            _right_model.layer2,
+            _right_model.layer3,
+            _right_model.layer4,
+            _right_model.avgpool
+        )
+
+        for param in self.left_features.parameters():
+            param.requires_grad = True
+        for param in self.right_features.parameters():
+            param.requires_grad = True
+
+        _num_ftrs = _left_model.fc.in_features + _right_model.fc.in_features + 2  # left, right and head_pose
+        self.classifier = GazeEstimationAbstractModel._create_fc_layers(num_out=num_out, num_features=_num_ftrs)
+        GazeEstimationAbstractModel._init_weights(self.modules())
+
+    def forward(self, left_eye, right_eye, headpose):
+        left_x = self.left_features(left_eye)
+        left_x = torch.flatten(left_x, 1)
+
+        right_x = self.right_features(right_eye)
+        right_x = torch.flatten(right_x, 1)
+
+        concat = torch.cat((left_x, right_x, headpose), dim=1)
+
+        fc_output = self.classifier(concat)
+
+        return fc_output
+
+
+class GazeEstimationmodelResneXt(GazeEstimationAbstractModel):
+
+    def __init__(self, num_out=2):
+        super(GazeEstimationmodelResneXt, self).__init__()
+        _left_model = models.resnext50_32x4d(pretrained=True)
+        _right_model = models.resnext50_32x4d(pretrained=True)
+
+        # remove the last ConvBRelu layer
+        self.left_features = nn.Sequential(
+            _left_model.conv1,
+            _left_model.bn1,
+            _left_model.relu,
+            _left_model.maxpool,
+            _left_model.layer1,
+            _left_model.layer2,
+            _left_model.layer3,
+            _left_model.layer4,
+            _left_model.avgpool
+        )
+
+        self.right_features = nn.Sequential(
+            _right_model.conv1,
+            _right_model.bn1,
+            _right_model.relu,
+            _right_model.maxpool,
+            _right_model.layer1,
+            _right_model.layer2,
+            _right_model.layer3,
+            _right_model.layer4,
+            _right_model.avgpool
         )
 
         for param in self.left_features.parameters():
@@ -250,10 +389,13 @@ if __name__ == "__main__":
     trans_left_img = trans(left_img).unsqueeze(0).cuda()
     trans_right_img = trans(right_img).unsqueeze(0).cuda()
 
-    model = GazeEstimationmodelResnet18()
+    model = GazeEstimationmodelResneXt()
     model = model.cuda()
     model.eval()
     start_time = time.time()
     for _ in trange(5000):
-        model(trans_left_img, trans_right_img, head_pose_gen)
+        _res = [model(trans_left_img, trans_right_img, head_pose_gen)]
+        _res = torch.stack(_res, dim=1)
+        _res = torch.mean(_res, dim=1)
+
     print("Evaluation Frequency: {:.3f}Hz".format(1.0 / ((time.time() - start_time) / 5000.0)))
