e173451b2c075acad3ce6e9d6fd3bad1b35e2748
Tobias Fischer <info@tobiasfischer.info>
2020-06-23 07:41:42 +1000

    Split download_models into several methods

--- rt_gene/scripts/download_models.py
+++ rt_gene/scripts/download_models.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-# Licensed under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode)
+# Licensed under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
+# (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode)
 
 from __future__ import print_function, division, absolute_import
 
@@ -35,28 +36,11 @@ def request_if_not_exist(file_name, url, md5sum=None, chunksize=1024):
                 file_name)
 
 
-if __name__ == '__main__':
-    request_if_not_exist(
-        os.path.join(os.path.dirname(os.path.realpath(__file__)), '../model_nets/ThreeDDFA/w_shp_sim.npy'),
-        'https://github.com/cleardusk/3DDFA/blob/master/train.configs/w_shp_sim.npy?raw=true',
-        '74d41d465580924456defd99401cfcdd')
-    request_if_not_exist(
-        os.path.join(os.path.dirname(os.path.realpath(__file__)), '../model_nets/ThreeDDFA/w_exp_sim.npy'),
-        'https://github.com/cleardusk/3DDFA/blob/master/train.configs/w_exp_sim.npy?raw=true',
-        '7566556b7760b8691f3a6fddbdd38fcc')
-    request_if_not_exist(
-        os.path.join(os.path.dirname(os.path.realpath(__file__)), '../model_nets/phase1_wpdc_vdc.pth.tar'),
-        'https://github.com/cleardusk/3DDFA/blob/master/models/phase1_wpdc_vdc.pth.tar?raw=true',
-        '01054c039b12b1b5f6e34e1fcf44fbf6')
+def download_gaze_tensorflow_models():
     request_if_not_exist(
         os.path.join(os.path.dirname(os.path.realpath(__file__)), '../model_nets/Model_allsubjects1.h5'),
         'https://imperialcollegelondon.box.com/shared/static/zu424pzptmw1klh70jsc697b37h7mwif.h5',
         'e55ea59d494d66dd075bf1503a32f99c')
-    request_if_not_exist(
-        os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                     '../model_nets/dlib_face_recognition_resnet_model_v1.dat'),
-        'https://imperialcollegelondon.box.com/shared/static/7zfltrwhrss0zsq2d2z0mgbz4j3ij3fk.dat',
-        '2316b25ae80acf4ad9b620b00071c423')
     request_if_not_exist(
         os.path.join(os.path.dirname(os.path.realpath(__file__)), '../model_nets/all_subjects_mpii_prl_utmv_0_02.h5'),
         "https://imperialcollegelondon.box.com/shared/static/5cjnijpo8qxawbkik0gjrmyc802j2h1v.h5",
@@ -73,16 +57,9 @@ if __name__ == '__main__':
         os.path.join(os.path.dirname(os.path.realpath(__file__)), '../model_nets/all_subjects_mpii_prl_utmv_3_02.h5'),
         "https://imperialcollegelondon.box.com/shared/static/hmcoxopu4xetic5bm47xqrl5mqktpg92.h5",
         "581f7a96ef88faf3a564aca083496dfa")
-    request_if_not_exist(
-        os.path.join(os.path.dirname(os.path.realpath(__file__)), '../model_nets/SFD/s3fd_facedetector.pth'),
-        "https://imperialcollegelondon.box.com/shared/static/wgfkq3pyzzuewiiwq0pzj0xiebolvlju.pth",
-        "3b5a9888bf0beb93c177db5a18375a6c")
-    request_if_not_exist(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../model_nets/blink_model_1.h5'),
-                         "https://imperialcollegelondon.box.com/shared/static/lke3k5f86qnfchzfh6lpon3isniqvkpz.h5",
-                         "75aab57645faed3beaba5dedfd0f3d36")
-    request_if_not_exist(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../model_nets/blink_model_2.h5'),
-                         "https://imperialcollegelondon.box.com/shared/static/x4u8c5mr468r6wzki93v45jemf3sz0r5.h5",
-                         "ed994ea8384a7894dac04926601d06ff")
+
+
+def download_gaze_pytorch_models():
     request_if_not_exist(
         os.path.join(os.path.dirname(os.path.realpath(__file__)), '../model_nets/Model_allsubjects1_pytorch.model'),
         "https://imperialcollegelondon.box.com/shared/static/zblg37jitf9q245k3ytad8nv814nz9o8.model",
@@ -109,3 +86,43 @@ if __name__ == '__main__':
                      '../model_nets/Model_prl_mpii_allsubjects2_pytorch.model'),
         "https://imperialcollegelondon.box.com/shared/static/nlkqe42rch2so4k5fb9ql40jjxsze0bh.model",
         "87e13756863393a94a8ffc4a75f2fca2")
+
+
+def download_blink_models():
+    request_if_not_exist(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../model_nets/blink_model_1.h5'),
+                         "https://imperialcollegelondon.box.com/shared/static/lke3k5f86qnfchzfh6lpon3isniqvkpz.h5",
+                         "75aab57645faed3beaba5dedfd0f3d36")
+    request_if_not_exist(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../model_nets/blink_model_2.h5'),
+                         "https://imperialcollegelondon.box.com/shared/static/x4u8c5mr468r6wzki93v45jemf3sz0r5.h5",
+                         "ed994ea8384a7894dac04926601d06ff")
+
+
+def download_external_landmark_models():
+    request_if_not_exist(
+        os.path.join(os.path.dirname(os.path.realpath(__file__)), '../model_nets/ThreeDDFA/w_shp_sim.npy'),
+        'https://github.com/cleardusk/3DDFA/blob/master/train.configs/w_shp_sim.npy?raw=true',
+        '74d41d465580924456defd99401cfcdd')
+    request_if_not_exist(
+        os.path.join(os.path.dirname(os.path.realpath(__file__)), '../model_nets/ThreeDDFA/w_exp_sim.npy'),
+        'https://github.com/cleardusk/3DDFA/blob/master/train.configs/w_exp_sim.npy?raw=true',
+        '7566556b7760b8691f3a6fddbdd38fcc')
+    request_if_not_exist(
+        os.path.join(os.path.dirname(os.path.realpath(__file__)), '../model_nets/phase1_wpdc_vdc.pth.tar'),
+        'https://github.com/cleardusk/3DDFA/blob/master/models/phase1_wpdc_vdc.pth.tar?raw=true',
+        '01054c039b12b1b5f6e34e1fcf44fbf6')
+    request_if_not_exist(
+        os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                     '../model_nets/dlib_face_recognition_resnet_model_v1.dat'),
+        'https://imperialcollegelondon.box.com/shared/static/7zfltrwhrss0zsq2d2z0mgbz4j3ij3fk.dat',
+        '2316b25ae80acf4ad9b620b00071c423')
+    request_if_not_exist(
+        os.path.join(os.path.dirname(os.path.realpath(__file__)), '../model_nets/SFD/s3fd_facedetector.pth'),
+        "https://imperialcollegelondon.box.com/shared/static/wgfkq3pyzzuewiiwq0pzj0xiebolvlju.pth",
+        "3b5a9888bf0beb93c177db5a18375a6c")
+
+
+if __name__ == '__main__':
+    download_gaze_tensorflow_models()
+    download_gaze_pytorch_models()
+    download_blink_models()
+    download_external_landmark_models()
