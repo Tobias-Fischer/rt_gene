ae2e0f0cb1f1678e57cc1ecde70dfed7e5c9fe73
ahmed-alhindawi <a.al-hindawi@imperial.ac.uk>
2021-02-10 14:34:44 +0000

    - added inference blink estimator in ROS format and standalone format

--- rt_bene_standalone/estimate_blink_standalone.py
+++ rt_bene_standalone/estimate_blink_standalone.py
@@ -10,8 +10,6 @@ from os import listdir
 import cv2
 import numpy as np
 
-from rt_bene.estimate_blink_base import BlinkEstimatorBase
-
 script_path = os.path.dirname(os.path.realpath(__file__))
 
 
@@ -26,43 +24,10 @@ def str2bool(v):
         raise argparse.ArgumentTypeError('Boolean value expected.')
 
 
-class BlinkEstimatorStandalone(BlinkEstimatorBase):
-    def __init__(self, model_path, threshold, input_size, viz):
-        super(BlinkEstimatorStandalone, self).__init__("gpu", model_path, threshold, input_size)
+class BlinkEstimatorFolderPair(object):
+    def __init__(self, blink_estimator, viz):
+        self.blink_estimator = blink_estimator
         self.viz = viz
-
-    def load_img(self, img_path, flip):
-        img = cv2.imread(img_path, cv2.IMREAD_COLOR)
-        if img is None:
-            print("ERROR: can't read " + img_path)
-        if flip:
-            img = cv2.flip(img, 1)
-        img = self.resize_img(img)
-        return img
-
-
-class BlinkEstimatorImagePair(BlinkEstimatorStandalone):
-    def __init__(self, model_path, threshold, input_size, viz):
-        super(BlinkEstimatorImagePair, self).__init__(model_path, threshold, input_size, viz)
-
-    def estimate(self, left_image_path, right_image_path):
-        left_image = [self.load_img(left_image_path, False)]
-        right_image = [self.load_img(right_image_path, True)]
-        start_time = time.time()
-        probs, blinks = self.predict(left_image, right_image)
-        print("Frequency: {:.2f}Hz".format(1 / (time.time() - start_time)))
-
-        if self.viz:
-            pair_img = np.concatenate((right_image[0], left_image[0]), axis=1)
-            viz_img = self.overlay_prediction_over_img(pair_img, blinks[0])
-            cv2.imshow('image pair visualisation', viz_img)
-            cv2.waitKey(0)
-        print("Blink: %s (p=%.3f)" % (blinks[0][0], probs[0]))
-
-
-class BlinkEstimatorFolderPair(BlinkEstimatorStandalone):
-    def __init__(self, model_path, threshold, input_size, viz):
-        super(BlinkEstimatorFolderPair, self).__init__(model_path, threshold, input_size, viz)
 
     def estimate(self, left_folder_path, right_folder_path):
         left_image_paths, right_image_paths = [], []
@@ -71,38 +36,52 @@ class BlinkEstimatorFolderPair(BlinkEstimatorStandalone):
         for left_image_name in sorted(listdir(left_folder_path)):
             left_image_path = left_folder_path + '/' + left_image_name
             left_image_paths.append(left_image_path)
-            left_images.append(self.load_img(left_image_path, False))
+            left_images.append(cv2.imread(left_image_path, cv2.IMREAD_COLOR))
 
         for right_image_name in sorted(listdir(right_folder_path)):
             right_image_path = right_folder_path + '/' + right_image_name
             right_image_paths.append(right_image_path)
-            right_images.append(self.load_img(right_image_path, True))
+            right_images.append(cv2.imread(right_image_path, cv2.IMREAD_COLOR))
+
+        l_images_input, r_images_input = [], []
+        for l_img, r_img in zip(left_images, right_images):
+            l_img_input, r_img_input = self.blink_estimator.inputs_from_images(l_img, r_img)
+            l_images_input.append(l_img_input)
+            r_images_input.append(r_img_input)
 
         start_time = time.time()
-        probs, blinks = self.predict(left_images, right_images)
-        print("Estimated blink for {} eye-image pairs, Time: {:.5f}s".format(len(left_images), time.time() - start_time))
+        probs = self.blink_estimator.predict(l_images_input, r_images_input)
+        blinks = probs >= self.blink_estimator.threshold
+        print(
+            "Estimated blink for {} eye-image pairs, Time: {:.5f}s".format(len(left_images), time.time() - start_time))
         if self.viz:
             for left_image, right_image, is_blinking in zip(left_images, right_images, blinks):
                 pair_img = np.concatenate((right_image, left_image), axis=1)
-                viz_img = self.overlay_prediction_over_img(pair_img, is_blinking)
+                viz_img = self.blink_estimator.overlay_prediction_over_img(pair_img, is_blinking)
                 cv2.imshow('folder images visualisation', viz_img)
                 cv2.waitKey(0)
         for left_image, right_image, p, is_blinking in zip(left_image_paths, right_image_paths, probs, blinks):
             print("Blink: %s (p=%.3f) for image pair: %20s %20s" % ("Yes" if is_blinking else "No ", p,
-                                                                    os.path.basename(left_image), os.path.basename(right_image)))
+                                                                    os.path.basename(left_image),
+                                                                    os.path.basename(right_image)))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Estimate blink from image or folder pair.')
     parser.add_argument('--left', type=str, help='Path to a left eye image or a directory containing left eye images',
                         default=os.path.join(script_path, './samples_blink/left/'))
-    parser.add_argument('--right', type=str, help='Path to a right eye image or a directory containing images right eye images',
+    parser.add_argument('--right', type=str,
+                        help='Path to a right eye image or a directory containing images right eye images',
                         default=os.path.join(script_path, './samples_blink/right/'))
     parser.add_argument('--model', nargs='+', type=str,
-                        default=[os.path.abspath(os.path.join(script_path, '../rt_gene/model_nets/blink_model_1.h5'))],
+                        default=[os.path.abspath(os.path.join(script_path, '../rt_gene/model_nets/blink_model_resnet50_f0.model')),
+                                 os.path.abspath(os.path.join(script_path, '../rt_gene/model_nets/blink_model_resnet50_f1.model')),
+                                 os.path.abspath(os.path.join(script_path, '../rt_gene/model_nets/blink_model_resnet50_f1.model'))],
                         help='List of blink estimators')
     parser.add_argument('--threshold', type=float, default=0.5,
                         help='Threshold to determine weither the prediction is positive or negative')
+    parser.add_argument('--device_id', type=str, default="cuda")
+    parser.add_argument('--blink_backend', type=str, choices=['tensorflow', 'pytorch'], default='pytorch')
     parser.add_argument('--vis_blink', type=str2bool, nargs='?', default=True,
                         help='Show the overlayed result on original image or not')
 
@@ -110,13 +89,21 @@ if __name__ == '__main__':
     left_path = args.left
     right_path = args.right
 
-    if os.path.isfile(left_path) and os.path.isfile(right_path):
-        blink_estimator = BlinkEstimatorImagePair(args.model, args.threshold, input_size=(96, 96), viz=args.vis_blink)
-    elif os.path.isdir(left_path) and os.path.isdir(right_path):
-        blink_estimator = BlinkEstimatorFolderPair(args.model, args.threshold, input_size=(96, 96), viz=args.vis_blink)
+    if args.blink_backend == "tensorflow":
+        from rt_bene.estimate_blink_tensorflow import BlinkEstimatorTensorflow
+        blink_estimator = BlinkEstimatorTensorflow(device_id_blink=args.device_id, threshold=0.425, model_files=args.model)
+    elif args.blink_backend == "pytorch":
+        from rt_bene.estimate_blink_pytorch import BlinkEstimatorPytorch
+        blink_estimator = BlinkEstimatorPytorch(device_id_blink=args.device_id, threshold=0.425, model_files=args.model)
     else:
-        raise Exception('Files / folders not found: Check that ' + left_path + ' and ' + right_path + ' exist')
+        raise Exception("Unknown backend")
 
-    blink_estimator.estimate(left_path, right_path)
-    if args.vis_blink:
-        cv2.destroyAllWindows()
+    if os.path.isdir(left_path) and os.path.isdir(right_path):
+        blink_folder = BlinkEstimatorFolderPair(blink_estimator, viz=args.vis_blink)
+        blink_folder.estimate(left_path, right_path)
+        if args.vis_blink:
+            cv2.destroyAllWindows()
+    else:
+        raise Exception('Folders not found: Check that ' + left_path + ' and ' + right_path + ' exist')
+
+
--- rt_gene/scripts/estimate_blink.py
+++ rt_gene/scripts/estimate_blink.py
@@ -16,7 +16,6 @@ import rospkg
 from rt_gene.msg import MSG_SubjectImagesList
 from rt_gene.msg import MSG_BlinkList, MSG_Blink
 from rt_gene.subject_ros_bridge import SubjectListBridge
-from rt_bene.estimate_blink_base import BlinkEstimatorBase
 
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
@@ -27,13 +26,22 @@ import collections
 from tqdm import tqdm
 
 
-class BlinkEstimatorNode(BlinkEstimatorBase):
+class BlinkEstimatorROS(object):
     def __init__(self, device_id_blink, model_files, threshold):
-        super(BlinkEstimatorNode, self).__init__(device_id_blink, model_files, threshold, (96, 96))
-        self.sub = rospy.Subscriber("/subjects/images", MSG_SubjectImagesList, self.callback, queue_size=1, buff_size=2**24)
         self.cv_bridge = CvBridge()
         self.bridge = SubjectListBridge()
         self.viz = rospy.get_param("~viz", True)
+
+        self.blink_backend = rospy.get_param("~blink_backend", default="tensorflow")
+
+        if self.blink_backend == "tensorflow":
+            from rt_bene.estimate_blink_tensorflow import BlinkEstimatorTensorflow
+            self._blink_estimator = BlinkEstimatorTensorflow(device_id_blink, model_files, threshold)
+        elif self.blink_backend == "pytorch":
+            from rt_bene.estimate_blink_pytorch import BlinkEstimatorPytorch
+            self._blink_estimator = BlinkEstimatorPytorch(device_id_blink, model_files, threshold)
+        else:
+            raise ValueError("Incorrect gaze_base backend, choices are: tensorflow or pytorch")
 
         self._last_time = rospy.Time().now()
         self._freq_deque = collections.deque(maxlen=30)  # average frequency statistic over roughly one second
@@ -43,19 +51,23 @@ class BlinkEstimatorNode(BlinkEstimatorBase):
         if self.viz:
             self.viz_pub = rospy.Publisher(rospy.get_param("~viz_topic", "/subjects/blink_images"), Image, queue_size=3)
 
+        self.sub = rospy.Subscriber("/subjects/images", MSG_SubjectImagesList, self.callback, queue_size=1,
+                                    buff_size=2 ** 24)
+
     def callback(self, msg):
         subjects = self.bridge.msg_to_images(msg)
         left_eyes = []
         right_eyes = []
 
         for subject in subjects.values():
-            left_eyes.append(self.resize_img(subject.left))
-            right_eyes.append(cv2.flip(self.resize_img(subject.right), 1))
-            
+            _left, _right = self._blink_estimator.inputs_from_images(subject.left, subject.right)
+            left_eyes.append(_left)
+            right_eyes.append(_right)
+
         if len(left_eyes) == 0:
             return
 
-        probs, _ = self.predict(left_eyes, right_eyes)
+        probs = self._blink_estimator.predict(left_eyes, right_eyes)
 
         self.publish_msg(msg.header, subjects, probs)
 
@@ -63,7 +75,7 @@ class BlinkEstimatorNode(BlinkEstimatorBase):
             blink_image_list = []
             for subject, p in zip(subjects.values(), probs):
                 resized_face = cv2.resize(subject.face, dsize=(224, 224), interpolation=cv2.INTER_CUBIC)
-                blink_image_list.append(self.overlay_prediction_over_img(resized_face, p))
+                blink_image_list.append(self._blink_estimator.overlay_prediction_over_img(resized_face, p))
 
             if len(blink_image_list) > 0:
                 blink_viz_img = self.cv_bridge.cv2_to_imgmsg(np.hstack(blink_image_list), encoding="bgr8")
@@ -72,13 +84,15 @@ class BlinkEstimatorNode(BlinkEstimatorBase):
 
         _now = rospy.Time().now()
         timestamp = msg.header.stamp
+
         _freq = 1.0 / (_now - self._last_time).to_sec()
         self._freq_deque.append(_freq)
         self._latency_deque.append(_now.to_sec() - timestamp.to_sec())
         self._last_time = _now
         tqdm.write(
             '\033[2K\033[1;32mTime now: {:.2f} message color: {:.2f} latency: {:.2f}s for {} subject(s) {:.0f}Hz\033[0m'.format(
-                (_now.to_sec()), timestamp.to_sec(), np.mean(self._latency_deque), len(subjects), np.mean(self._freq_deque)), end="\r")
+                (_now.to_sec()), timestamp.to_sec(), np.mean(self._latency_deque), len(subjects),
+                np.mean(self._freq_deque)), end="\r")
 
     def publish_msg(self, header, subjects, probabilities):
         blink_msg_list = MSG_BlinkList()
@@ -86,19 +100,20 @@ class BlinkEstimatorNode(BlinkEstimatorBase):
         for subject_id, p in zip(subjects.keys(), probabilities):
             blink_msg = MSG_Blink()
             blink_msg.subject_id = str(subject_id)
-            blink_msg.blink = bool(p >= self.threshold)
+            blink_msg.blink = bool(p >= self._blink_estimator.threshold)
             blink_msg.probability = p
             blink_msg_list.subjects.append(blink_msg)
-            
+
         self.blink_publisher.publish(blink_msg_list)
 
 
 if __name__ == "__main__":
     try:
         rospy.init_node("blink_estimator")
-        blink_detector = BlinkEstimatorNode(device_id_blink=rospy.get_param("~device_id_blinkestimation", "/gpu:0"),
-                                            model_files=[os.path.join(rospkg.RosPack().get_path("rt_gene"), model_file) for model_file in rospy.get_param("~model_files")],
-                                            threshold=rospy.get_param("~threshold", 0.5))
+        blink_detector = BlinkEstimatorROS(device_id_blink=rospy.get_param("~device_id_blinkestimation", "/gpu:0"),
+                                           model_files=[os.path.join(rospkg.RosPack().get_path("rt_gene"), model_file)
+                                                        for model_file in rospy.get_param("~model_files")],
+                                           threshold=rospy.get_param("~threshold", 0.5))
         rospy.spin()
     except rospy.exceptions.ROSInterruptException:
         print("See ya")
